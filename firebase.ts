93ffac8363487b37abe9cecea3aaf22f6f12ccce
Manoj <manojkumarmondal554@gmail.com>
2022-05-10 22:03:48 +0530

    firebase file modified

--- firebase.ts
+++ firebase.ts
@@ -1,22 +1,20 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwfJh6raF5iGSmcMaSQqLlHeIVcAe2bK8",
-    authDomain: "netflix-clone-fc985.firebaseapp.com",
-    projectId: "netflix-clone-fc985",
-    storageBucket: "netflix-clone-fc985.appspot.com",
-    messagingSenderId: "149139696923",
-    appId: "1:149139696923:web:5bbbf2b11b69251f11b7be",
-    measurementId: "G-SH40FML9HT"
+    apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
+    authDomain: `${process.env.NEXT_PUBLIC_FB_APP_NAME}.firebaseapp.com`,
+    projectId: process.env.NEXT_PUBLIC_FB_APP_NAME,
+    storageBucket: `${process.env.NEXT_PUBLIC_FB_APP_NAME}.appspot.com`,
+    messagingSenderId: process.env.NEXT_PUBLIC_FB_MSI,
+    appId: process.env.NEXT_PUBLIC_FB_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-// const analytics = getAnalytics(app);
 const db = getFirestore()
 const auth = getAuth()
 
